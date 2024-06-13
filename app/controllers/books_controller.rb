d04416dfa64fc741e341e9c803f77d09eb517e69
EC2 Default User <ec2-user@ip-172-31-43-240.ap-northeast-1.compute.internal>
2024-06-13 12:01:30 +0000

    [Finish22]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,17 +17,26 @@ class BooksController < ApplicationController
 
   def create
     @book=Book.new(book_params)
-    @book.save
-    redirect_to "/books"
-    redirect_to book_path(book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
-  private
-  def book_params
-    params.permit(:title,:body)
-  end
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
+  end
+  private
+  def book_params
+    params.permit(:title,:body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post "books" => "books#create"
   resources :all_books, only: [:index, :create, :update]
   patch "books/:id" => "books#update",as: "update_book"
+  delete "books/:id" => "books#destroy",as: "destroy_book"
   get "books/:id" =>"books#show",as: "book"
   get "books/:id/edit" =>"books#edit",as: "edit_book"
 end
