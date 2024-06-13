79982d6ca45d93ff68d57e9df24c1fda5617bad1
EC2 Default User <ec2-user@ip-172-31-43-240.ap-northeast-1.compute.internal>
2024-06-13 00:09:22 +0000

    [20240613]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,18 +8,26 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
     @book=Book.new(book_params)
     @book.save
     redirect_to "/books"
+    redirect_to book_path(book.id)
   end
   private
   def book_params
     params.permit(:title,:body)
+  end
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   get 'books/new'
   get 'books/index'
-  get 'books/sohw'
-  get 'books/edit'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/top" => "homes#top"
   get "/books" => "books#index"
-  get "/show" =>"books#show"
-  get "/show/edit" =>"books#edit"
-  post "book" => "books#create"
-  resources :books, only: [:index, :create]
+  post "books" => "books#create"
+  resources :books, only: [:index, :create, :update]
+  patch "books/:id" => "books#update",as: "update_book"
+  get "books/:id" =>"books#show",as: "book"
+  get "books/:id/edit" =>"books#edit",as: "edit_book"
 end
